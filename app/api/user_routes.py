198e131ffb611300017fb7824625bb8764c8f6bc
Izzy <snakedreamz134@gmail.com>
2021-12-15 12:22:51 -0500

    added post and get route for post table

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import User
+from app.models import db, User, Post
+from app.forms import NewPost, EditPost
+from .auth_routes import validation_errors_to_error_messages
 
 user_routes = Blueprint('users', __name__)
 
@@ -17,3 +19,24 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+
+@user_routes.route('/<int:id>/posts')
+def get_posts(id):
+    posts = Post.query.filter(Post.user_id == id)
+    return {'posts': [post.to_dict() for post in posts]}
+
+
+@user_routes.route('/<int:id>/posts', methods=['POST'])
+def create_post(id):
+    form = NewPost()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        newPost = Post(user_id=form['user_id'].data,
+                       content=form['content'].data, image_url=form['image_url'].data)
+
+        db.session.add(newPost)
+        db.session.commit()
+        return newPost.to_dict()
+    else:
+        return {'errors': validation_errors_to_error_messages(form.errors)}, 400
