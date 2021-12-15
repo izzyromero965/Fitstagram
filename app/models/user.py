83d1481d0f8046acfc81ec86dd94c785e006be33
Izzy <snakedreamz134@gmail.com>
2021-12-15 11:19:41 -0500

    updated users table

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    nick_name = db.Column(db.String(50), nullable=False)
+    profile_image_url = db.Column(db.String)
 
     @property
     def password(self):
@@ -26,5 +28,7 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'nick_name': self.nick_name,
+            'profile_image_url': self.profile_image_url
         }
