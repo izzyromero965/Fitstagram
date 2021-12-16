b486ced44e12220cdb0212e0b40c1b46ac5c799d
Izzy <snakedreamz134@gmail.com>
2021-12-16 12:44:57 -0500

    added actions, thunks and reducer for posts. also modified some backend routes to make a little bit more sense

---
+++ react-app/src/store/post.js
@@ -0,0 +1,137 @@
+const CREATE_POST = 'post/CREATE_POST';
+const GET_ALL_POSTS = 'post/GET_POSTS';
+const GET_USERS_POSTS = 'post/GET_USERS_POSTS';
+const DELETE_POST = 'post/DELETE_POST';
+const EDIT_POST = 'post/EDIT_POST';
+
+const newPost = (post) => ({
+  type: CREATE_POST,
+  post,
+});
+
+const getAllPosts = (posts) => ({
+  type: GET_ALL_POSTS,
+  posts,
+});
+
+const getUsersPosts = (posts) => ({
+  type: GET_USERS_POSTS,
+  posts,
+});
+
+const editPost = (post) => ({
+  type: EDIT_POST,
+  post,
+});
+
+const deletePost = (post) => ({
+  type: DELETE_POST,
+  post,
+});
+
+// Creates a post
+export const createPost = (formdata, id) => async (dispatch) => {
+  const response = await fetch(`/api/users/${id}/posts/new`, {
+    method: 'POST',
+    body: formdata,
+  });
+
+  if (response.ok) {
+    const post = await response.json();
+    dispatch(newPost(post));
+  } else if (response.status < 500) {
+    const data = await response.json();
+    if (data.errors) {
+      return data.errors;
+    }
+  } else {
+    return ['An error occurred. Please try again.'];
+  }
+};
+
+// Gets a single users posts
+export const getUsersPosts = (id) => async (dispatch) => {
+  const response = await fetch(`/api/users/${id}/posts`);
+  if (response.ok) {
+    const posts = await response.json();
+    dispatch(getUsersPosts(posts));
+    return posts;
+  }
+};
+
+// Gets all posts
+export const getPosts = () => async (dispatch) => {
+  const response = await fetch('/api/users/posts');
+  if (response.ok) {
+    const posts = await response.json();
+    dispatch(getAllPosts(posts));
+    return posts;
+  }
+};
+
+//Edits a single post
+export const editOnePost = (post) => async (dispatch) => {
+  const response = await fetch(
+    `/api/users/${post.user_id}/posts/${post.id}/edit`,
+    {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(post),
+    }
+  );
+  if (response.ok) {
+    const edited = await response.json();
+    dispatch(editPost(edited));
+    return edited;
+  }
+};
+
+//Delete a post
+export const deletePost = (userid, id) => async (dispatch) => {
+  const response = await fetch(`/api/users/${userid}/posts/${id}/delete`, {
+    method: 'DELETE',
+  });
+  if (response.ok) {
+    const post = await response.json();
+    dispatch(deletePost(post));
+    return post;
+  }
+};
+
+const initialState = {};
+
+const postReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_POST: {
+      const newState = {
+        ...state,
+        [action.post.id]: action.post,
+      };
+      return newState;
+    }
+    case GET_ALL_POSTS: {
+      const newState = {
+        ...state,
+        ...action.posts.posts,
+      };
+      return newState;
+    }
+    case EDIT_POST: {
+      const newState = {
+        ...state,
+        [action.post.id]: action.post,
+      };
+      return newState;
+    }
+
+    case DELETE_POST: {
+      const newState = {
+        ...state,
+      };
+      delete newState[action.post.id];
+      return newState;
+    }
+  }
+};
+
+export default postReducer;
