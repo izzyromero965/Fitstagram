70c999df6822221c1ab1e1895f849bf825a7a98d
Izzy <snakedreamz134@gmail.com>
2021-12-31 19:26:06 -0500

    styling search results

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -28,17 +28,19 @@ const Search = () => {
       <div style={showResults} className="results">
         {searchTerm !== '' &&
           results?.map((user) => (
-            <a
-              key={user?.id}
-              href={`/users/${user.id}`}
-              className="results-link"
-            >
-              <img src={user?.profile_image_url} className="search-pic" />
-              <div>
-                <div>{user?.username}</div>
-                <div>{user?.nick_name}</div>
-              </div>
-            </a>
+            <div className="results-container">
+              <a
+                key={user?.id}
+                href={`/users/${user.id}`}
+                className="results-link"
+              >
+                <img src={user?.profile_image_url} className="search-pic" />
+                <div className="results-desc-container">
+                  <div>{user?.username}</div>
+                  <div>{user?.nick_name}</div>
+                </div>
+              </a>
+            </div>
           ))}
       </div>
     </div>
